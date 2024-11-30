dcde365fc79aa2beff2041d11ba2889960cc6b05
Sasan <77465683+sasansabour@users.noreply.github.com>
2024-11-30 01:50:54 -0500

    Add files via upload

---
+++ api-gateway/APIGateway.py
@@ -0,0 +1,54 @@
+import random
+from flask import Flask, request, jsonify
+import requests
+import json
+
+
+app = Flask(__name__)
+
+# Configuration: Read P (percentage of traffic to v1) from a configuration file or environment variable
+with open('config.json') as config_file:
+    config = json.load(config_file)
+    P = config.get('P', 80)  # Default to 80% to v1 if not specified
+
+
+# Define routes to forward requests
+@app.route('/user', methods=['POST', 'PUT'])
+@app.route('/user/<user_id>', methods=['PUT'])
+def user_service(user_id=None):
+    """Forward requests to the User Microservice"""
+    # Randomly route P% of traffic to v1 and (100 - P)% to v2
+    route_to_v1 = random.randint(1, 100) <= P
+
+    #if route_to_v1:
+    #    user_service_url = f"http://127.0.0.1:5001/user"
+    #else:
+    #    user_service_url = f"http://127.0.0.1:5003/user"
+
+    if route_to_v1:
+        user_service_url = f"http://35.223.205.182:5001/user/{user_id}" if user_id else f"http://35.223.205.182:5001/user"
+    else:
+        user_service_url = f"http://35.223.205.182:5003/user/{user_id}" if user_id else f"http://35.223.205.182:5003/user"
+
+    response = requests.request(
+        method=request.method,
+        url=user_service_url,#f"http://localhost:5001/{path}",
+        json=request.json
+    )
+    return jsonify(response.json()), response.status_code
+
+
+@app.route('/order', methods=['GET', 'POST', 'PUT'])
+def order_service():
+    """Forward requests to the Order Microservice"""
+    print("hi")
+    if request.method == 'GET':
+        data = request.args.to_dict()
+        response = requests.get("http://35.223.205.182:5002/order", params=data)
+    else:
+        data = request.json
+        response = requests.request(method=request.method,url=f"http://35.223.205.182:5002/order",json=data)
+    return jsonify(response.json()), response.status_code
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
