e87c9635e9c200058ec75366d0c7ed17c6ed81c3
Sasan Sabour <77465683+sasansabour@users.noreply.github.com>
2025-08-21 11:16:23 -0400

    Update UserMicroserviceV2.py

--- user-microservice-v2/UserMicroserviceV2.py
+++ user-microservice-v2/UserMicroserviceV2.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
@@ -7,17 +8,21 @@ import json
 
 app = Flask(__name__)
 
+MONGO_USER = os.getenv('MONGO_USER')
+MONGO_PASSWORD = os.getenv('MONGO_PASSWORD')
+MONGO_CLUSTER = os.getenv('MONGO_CLUSTER')
+RABBITMQ_HOST = os.getenv('RABBITMQ_HOST', 'rabbitmq')
+
+
 users = {}
 
-password = "sS%4020242024"
-#escaped_password = urllib.parse.quote_plus(password)
-uri = f"mongodb+srv://sasansabour:{password}@cluster0.tuzcj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = f"mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_CLUSTER}/?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(uri, server_api=ServerApi('1'))
 db = client['user_database']  # The database
 users_collection = db['users']  # Collection for user data
 
 # RabbitMQ connection
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq',heartbeat=120, retry_delay=5, socket_timeout=60,
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST,heartbeat=120, retry_delay=5, socket_timeout=60,
         automatic_reconnect=True))
 channel = connection.channel()
 
@@ -29,7 +34,6 @@ channel.queue_declare(queue='user_update_queue')
 def create_user():
     data = request.json
     user_id = data['_id']
-    #users[user_id] = data
     users_collection.insert_one(data)
     return jsonify({"message": "User created successfully"}), 201
 
@@ -56,39 +60,6 @@ def update_user(user_id):
     else:
         return jsonify({"error": "User not found or no change detected"}), 404
 
-"""
-@app.route('/user/<user_id>/phone', methods=['PUT'])
-def update_user_phone_v2(user_id):
-    #data = request.json
-    phone = request.json.get('phone')
-    # Update user data in the users collection
-    result = users_collection.update_one({'_id': user_id}, {'$set': data})
-
-    if result.modified_count > 0:
-        # Publish the event to RabbitMQ
-        event = {
-            'user_id': user_id,
-            'updated_data': data
-        }
-        channel.basic_publish(
-            exchange='',
-            routing_key='user_update_queue',
-            body=json.dumps(event)
-        )
-
-        return jsonify({"message": "User updated and event published"}), 200
-    else:
-        return jsonify({"error": "User not found or no change detected"}), 404
-    
-    if user_id in users_v2:
-        phone = request.json.get('phone')
-        if phone:
-            users_v2[user_id]['phone'] = phone
-            return jsonify({"message": "Phone number updated successfully"}), 200
-        return jsonify({"error": "Phone number not provided"}), 400
-    else:
-        return jsonify({"error": "User not found"}), 404
-"""
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5003)
